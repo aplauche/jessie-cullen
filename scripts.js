5d09d7d87c4bd57e5326c2c8b20c3f0740d12c2f
Anton Plauche <antonplauche92@gmail.com>
2021-05-04 14:19:02 -0500

    finished with throttling function in place

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
 const headerTag = document.querySelector("header");
+const arrow = document.querySelector("img.arrow");
 const blobGroups = document.querySelectorAll("g.blob");
 const sectionTags = document.querySelectorAll("section");
 
@@ -10,6 +11,7 @@ const fadeHeader = function () {
   let pixels = window.pageYOffset;
   let height = window.innerHeight;
   headerTag.style.opacity = 1 - qubicEase(pixels / height);
+  arrow.style.opacity = 1 - qubicEase(pixels / height);
 };
 
 const checkBlobs = function () {
@@ -25,5 +27,15 @@ const checkBlobs = function () {
   });
 };
 
-window.addEventListener("scroll", fadeHeader);
-window.addEventListener("scroll", checkBlobs);
+function throttle(fn, wait) {
+  var time = Date.now();
+  return function () {
+    if (time + wait - Date.now() < 0) {
+      fn();
+      time = Date.now();
+    }
+  };
+}
+
+window.addEventListener("scroll", throttle(fadeHeader, 100));
+window.addEventListener("scroll", throttle(checkBlobs, 100));
